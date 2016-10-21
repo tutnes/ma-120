32a5fc5668db1f92a76edafb80ac355454e343e0
Tarjei Utnes <tutnes@gmail.com>
2016-10-21 23:20:08 +0200

    Safety commit

---
+++ excersise07/src/main/java/XmlInputFormat.java
@@ -0,0 +1,15 @@
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+
+public class XmlInputFormat extends TextInputFormat{
+
+    @Override
+    public RecordReader<LongWritable, Text> createRecordReader(InputSplit split, TaskAttemptContext context) {
+        return new XmlRecordReader();
+        //return new XmlRecordReader("<student ", "</student>");
+    }
+}
