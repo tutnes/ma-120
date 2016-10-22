1222c32f7bb6bbf458b5a10d2a3540a0aeb15edb
Tarjei Utnes <tutnes@gmail.com>
2016-10-22 23:59:03 +0200

    Made the first wordcount project, think it works

---
+++ exam/2.2.1/a_wordcount/src/main/java/org/hadoop_seed/wordcount/XmlRecordReader.java
@@ -0,0 +1,110 @@
+package org.hadoop_seed.wordcount;
+
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.io.DataOutputBuffer;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class XmlRecordReader extends RecordReader<LongWritable, Text> {
+    private byte[] tagName;
+    private byte[] tagNameEnd;
+    private long start;
+    private long end;
+    private FSDataInputStream inputStream;
+    private LongWritable currentKey;
+    private Text currentValue;
+    private final DataOutputBuffer buffer = new DataOutputBuffer();
+
+    public XmlRecordReader() {
+        try {
+            //this.tagName = ("<student ").getBytes("UTF-8");
+            //this.tagNameEnd = ("</student>").getBytes("UTF-8");
+            this.tagName = ("<student ").getBytes("UTF-8");
+            this.tagNameEnd = ("</student>").getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void initialize(InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
+        FileSplit filesplit = (FileSplit) split;
+        start = filesplit.getStart();
+        end = start + filesplit.getLength();
+        FileSystem fs = filesplit.getPath().getFileSystem(context.getConfiguration());
+        inputStream = fs.open(filesplit.getPath());
+        inputStream.seek(start);
+    }
+
+
+    @Override
+    public LongWritable getCurrentKey() throws IOException, InterruptedException {
+        return currentKey;
+    }
+
+    @Override
+    public Text getCurrentValue() throws IOException, InterruptedException {
+        return currentValue;
+    }
+
+    @Override
+    public float getProgress() throws IOException, InterruptedException {
+        //TODO
+        return 0.0f;
+    }
+
+    @Override
+    public boolean nextKeyValue() throws IOException, InterruptedException {
+        if (inputStream.getPos() < end) {
+            if (readUntilMatch(tagName, false)) {
+                try {
+                    buffer.write(tagName);
+                    if (readUntilMatch(tagNameEnd, true)) {
+                        //TODO
+                        return true;
+                    }
+                } finally {
+                    buffer.reset();
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean readUntilMatch(byte[] match, boolean appendBytes) throws IOException {
+        int position = 0;
+        while (true) {
+            int nextByte = inputStream.read();
+            if (nextByte == -1) {
+                return false;
+            }
+            if (appendBytes) {
+                buffer.write(nextByte);
+            }
+            if (nextByte == match[position]) {
+                position++;
+                if (position >= match.length) {
+                    return true;
+                }
+            } else {
+                position = 0;
+            }
+            if (!appendBytes && position == 0 && inputStream.getPos() >= end) {
+                return false;
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        //TODO
+    }
+}
