5304e1e33f9af672e19b9b08ece1faf812498973
Tarjei Utnes <tutnes@gmail.com>
2016-11-11 00:51:49 +0100

    Having some xml issues

--- exam/2.2.1/a_wordcount/src/main/java/org/hadoop_seed/wordcount/WordCountMapper.java
+++ exam/2.2.1/a_wordcount/src/main/java/org/hadoop_seed/wordcount/WordCountMapper.java
@@ -2,6 +2,7 @@ package org.hadoop_seed.wordcount;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.commons.lang.StringEscapeUtils;
@@ -18,21 +19,31 @@ public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritabl
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String output = value.toString().replaceAll("ody=\"",""); //Removes the leading tag
-        output = output.replaceAll("\"",""); //Removes the trailing tag
-        output = StringEscapeUtils.unescapeHtml(output); //Unescapes the html tags
-        output = output.replaceAll("\\<.*?>",""); //Removes html tags
-        output = output.replaceAll("\\W", " "); //Replaces non alphanumeric characters with space
-        output = output.replaceAll("\\d", " "); //Replaces digits with space
-        output = output.replaceAll("_", " "); //Replaces underscore with space
-        output = output.trim().replaceAll(" +", " "); //Removes any double spaces
 
-        //Iterates through the ouputstring, splits it in words
-        final String[] words = output.split(" ");
-        for (String word : words) {
+        if (value.toString().contains("PostTypeId=\"1\"")) { //If it is a question
+            
+            String output = value.toString(); //.replaceAll("ody=\"", ""); //Removes <></>he leading tag
+            Pattern pattern = Pattern.compile("^(.*?)abc");
+            Matcher matcher = pattern.matcher(output);
+            if (matcher.find()) {
+                output = matcher.group(1);
+            }
+            output = output.replaceAll("\"", ""); //Removes the trailing tag
+            output = StringEscapeUtils.unescapeHtml(output); //Unescapes the html tags
+            output = output.replaceAll("\\<.*?>", ""); //Removes html tags
+            output = output.replaceAll("\\W", " "); //Replaces non alphanumeric characters with space
+            output = output.replaceAll("\\d", " "); //Replaces digits with space
+            output = output.replaceAll("_", " "); //Replaces underscore with space
+            output = output.trim().replaceAll(" +", " "); //Removes any double spaces
+
+            //Iterates through the ouputstring, splits it in words
+            final String[] words = output.split(" ");
+            for (String word : words) {
 
                 context.write(new Text(word), UNO);
 
+            }
         }
+
     }
 }
