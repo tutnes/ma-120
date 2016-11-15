9deaab85801175b5a423449c4026c0bacd363eb1
Tarjei Utnes <tutnes@gmail.com>
2016-11-15 21:57:50 +0100

    Finished 2.2.2 a

--- exam/2.2.2/a_counting/src/main/java/org/hadoop_seed/wordcount/WordCountMapper.java
+++ exam/2.2.2/a_counting/src/main/java/org/hadoop_seed/wordcount/WordCountMapper.java
@@ -20,32 +20,11 @@ public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritabl
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
-        if (value.toString().contains("PostTypeId=\"1\"")) { //If it is a question
-            
-            String output = value.toString(); //.replaceAll("ody=\"", ""); //Removes <></>he leading tag
-            Pattern pattern = Pattern.compile("ody=\"(.*?)\"");
-    //            Pattern pattern = Pattern.compile("^(.*?)\"");
+        if (value.toString().contains("DisplayName=")) { //If it is a row with a user on ir
 
-            Matcher matcher = pattern.matcher(output);
-            if (matcher.find()) {
-                output = matcher.group(1);
-            }
-            //System.out.println(output); For debug purposes
-            output = output.replaceAll("\"", ""); //Removes the trailing tag
-            output = StringEscapeUtils.unescapeHtml(output); //Unescapes the html tags
-            output = output.replaceAll("\\<.*?>", ""); //Removes html tags
-            output = output.replaceAll("\\W", " "); //Replaces non alphanumeric characters with space
-            output = output.replaceAll("\\d", " "); //Replaces digits with space
-            output = output.replaceAll("_", " "); //Replaces underscore with space
-            output = output.trim().replaceAll(" +", " "); //Removes any double spaces
-            output = output.toLowerCase(); //Sets all characters to lower case
-            //Iterates through the ouputstring, splits it in words
-            final String[] words = output.split(" ");
-            for (String word : words) {
+                context.write(new Text("Usercount"), UNO);
 
-                context.write(new Text(word), UNO);
 
-            }
         }
 
     }
