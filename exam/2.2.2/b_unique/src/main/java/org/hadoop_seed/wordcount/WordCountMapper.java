8d0c2822272ed4091e054f229ea8f761d927904e
Tarjei Utnes <tutnes@gmail.com>
2016-11-16 08:24:03 +0100

    Safety commit

--- exam/2.2.2/b_unique/src/main/java/org/hadoop_seed/wordcount/WordCountMapper.java
+++ exam/2.2.2/b_unique/src/main/java/org/hadoop_seed/wordcount/WordCountMapper.java
@@ -20,32 +20,30 @@ public class WordCountMapper extends Mapper<LongWritable, Text, Text, NullWritab
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
-        if (value.toString().contains("PostTypeId=\"1\"")) { //If it is a question
+        if (value.toString().contains("isplayName=")) { //If it is a question
             
             String output = value.toString(); //.replaceAll("ody=\"", ""); //Removes <></>he leading tag
-            Pattern pattern = Pattern.compile("itle=\"(.*?)\"");
-    //            Pattern pattern = Pattern.compile("^(.*?)\"");
-
+            Pattern pattern = Pattern.compile("isplayName=\"(.*?)\"");
             Matcher matcher = pattern.matcher(output);
             if (matcher.find()) {
                 output = matcher.group(1);
             }
             //System.out.println(output); For debug purposes
             output = output.replaceAll("\"", ""); //Removes the trailing tag
-            output = StringEscapeUtils.unescapeHtml(output); //Unescapes the html tags
-            output = output.replaceAll("\\<.*?>", ""); //Removes html tags
-            output = output.replaceAll("\\W", " "); //Replaces non alphanumeric characters with space
-            output = output.replaceAll("\\d", " "); //Replaces digits with space
-            output = output.replaceAll("_", " "); //Replaces underscore with space
-            output = output.trim().replaceAll(" +", " "); //Removes any double spaces
-            output = output.toLowerCase(); //Sets all characters to lower case
+    //        output = StringEscapeUtils.unescapeHtml(output); //Unescapes the html tags
+ //           output = output.replaceAll("\\<.*?>", ""); //Removes html tags
+//            output = output.replaceAll("\\W", " "); //Replaces non alphanumeric characters with space
+//            output = output.replaceAll("\\d", " "); //Replaces digits with space
+//            output = output.replaceAll("_", " "); //Replaces underscore with space
+//            output = output.trim().replaceAll(" +", " "); //Removes any double spaces
+//            output = output.toLowerCase(); //Sets all characters to lower case
             //Iterates through the ouputstring, splits it in words
-            final String[] words = output.split(" ");
-            for (String word : words) {
+            //final String[] words = output.split(" ");
+            //for (String word : words) {
 
-                context.write(new Text(word), NullWritable.get());
+                context.write(new Text(output), NullWritable.get());
 
-            }
+ //           }
         }
 
     }
