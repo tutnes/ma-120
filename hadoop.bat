59e59fe964444d071b6be2c68e6d1eeeb5b7b99c
Tarjei Utnes <tutnes@gmail.com>
2016-11-18 17:29:21 +0100

    Removed some old stuff

--- hadoop.bat
+++
@@ -1,1 +0,0 @@
-docker run -it -v /C/github/ma-120/exam:/root/ma-120 naimdjon/hadoop
--- r/fileLength.R
+++
@@ -1,5 +0,0 @@
-fileLength <- function(fileNameurl) {
-  x = read.table(fileNameurl)
-  len = length(x[,1])
-  return (len)
-}
